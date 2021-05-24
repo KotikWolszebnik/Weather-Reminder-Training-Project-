f55b462a5ba6f1bb12efc78f6e763c63a5a86f5d
Shavrin Maksim <nutmegraw@gmail.com>
2021-05-24 12:45:59 +0500

    django project made

---
+++ DjangoWeatherRemider/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+# Create your views here.
+
+class SubscriptionCreateView(CreateAPIView):
+    serializer_class = 
+
