ba1ae68d188319c345ddc9f972710e2f53aaf9ca
Shavrin Maksim <nutmegraw@gmail.com>
2021-05-27 17:24:52 +0500

    custom user model deleted

--- DjangoWeatherRemider/api/models.py
+++ DjangoWeatherRemider/api/models.py
@@ -1,30 +1,16 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import (CASCADE, CharField, DateTimeField, EmailField,
-                              ForeignKey, IntegerField, Model)
-
+from django.db.models import (CASCADE, CharField, ForeignKey, IntegerField,
+                              Model)
+from django.contrib.auth.models import User
 # Create your models here.
-
-
-class Account(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['password']
-    username = None
-    email = EmailField(unique=True)
-    reg_confirmed_date = DateTimeField(auto_now=False, null=True)
-
-    def __str__(self):
-        return f'< Account : {self.email} >'
 
 
 class Subscription(Model):
     NOTIFICATION_PERIODS = [
-        (1, '1'),
-        (3, '3'),
-        (6, '6'),
-        (12, '12'),
-        (24, '24'),
+        (1, '1'), (3, '3'), (6, '6'), (12, '12'), (24, '24'),
     ]
-    account = ForeignKey(Account, on_delete=CASCADE, related_name='subscriptions')
+    user = ForeignKey(
+        User, on_delete=CASCADE, related_name='subscriptions',
+    )
     city = ForeignKey('City', on_delete=CASCADE)
     notification_period = IntegerField(choices=NOTIFICATION_PERIODS)
 
