828bde55dafa1726fd23c92c6754de07ebb70fda
Shavrin Maksim <nutmegraw@gmail.com>
2021-05-27 17:27:08 +0500

    URLs added

--- DjangoWeatherRemider/api/urls.py
+++ DjangoWeatherRemider/api/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from .views import SubscriptionView, CityView, register, confirm_registration
 
 urlpatterns = [
-    
+    path('auth/', include('rest_framework.urls')),
+    path('register/', register),
+    path('confirm/<str:unique_string>/', confirm_registration),
+    path('city/search_by_name/', CityView.as_view()),
+    path('city/<int:pk>/', CityView.as_view()),
+    path('subscription/all/', SubscriptionView.as_view()),
+    path('subscription/<int:pk>', SubscriptionView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
