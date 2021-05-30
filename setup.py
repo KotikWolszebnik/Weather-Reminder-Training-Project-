7613846c126f2afd0fc9494a82e61cc6fccb01db
Shavrin Maksim <nutmegraw@gmail.com>
2021-05-30 17:57:23 +0500

    changed for the new app

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='Djangogramm',  # Replace with your own username
+    name='DjangoWeatherReminder',  # Replace with your own username
     version='0.0.1',
     author="Maksim Shavrin",
     author_email="nutmegraw@gmail.com",
-    description="Social network like the Instagramm.",
+    description="API for the email weather reminding",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.foxminded.com.ua/orahmudri/task-12-create-basic-application",
+    url="https://git.foxminded.com.ua/orahmudri/task-17-create-basic-application",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
