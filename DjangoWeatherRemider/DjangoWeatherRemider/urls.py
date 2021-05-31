30d6fbab146c40f153da4355b9dd6c5eea024cfd
Shavrin Maksim <nutmegraw@gmail.com>
2021-05-31 15:09:52 +0500

    djoser  added

--- DjangoWeatherRemider/DjangoWeatherRemider/urls.py
+++ DjangoWeatherRemider/DjangoWeatherRemider/urls.py
@@ -18,5 +18,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path to djoser end points
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+
+    # path to our account's app endpoints
     path('api/v1/', include('api.urls')),
 ]
