513bd4ccb7a49025c03234501f5aa891be546e35
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-01 17:47:38 +0500

    RegisterSerializer added

--- DjangoWeatherRemider/api/serializers.py
+++ DjangoWeatherRemider/api/serializers.py
@@ -1,7 +1,25 @@
+from django.contrib.auth.models import User
 from rest_framework.serializers import (CurrentUserDefault, HiddenField,
                                         ModelSerializer)
 
-from .models import Subscription, City
+from .models import City, Subscription
+
+
+class RegisterSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def validate(self, attrs):
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['email'],
+            is_active=False,
+            **validated_data,
+        )
 
 
 class SubscriptionSerializer(ModelSerializer):
