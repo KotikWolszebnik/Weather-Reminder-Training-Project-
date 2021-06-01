c08c981100cd800c8f958c6b6572ba99fa9139ff
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-01 20:00:43 +0500

    DEFAULT_PERMISSION_CLASSES deleted

--- DjangoWeatherRemider/DjangoWeatherRemider/settings.py
+++ DjangoWeatherRemider/DjangoWeatherRemider/settings.py
@@ -145,9 +145,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'api.permissions.IsActive',
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
