535cf7a638563617dd43d81af64f3a33c34b6864
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-07 02:03:18 +0500

    ordering added

--- DjangoWeatherRemider/api/models.py
+++ DjangoWeatherRemider/api/models.py
@@ -2,18 +2,21 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db.models import (CASCADE, BooleanField, CharField, EmailField,
                               ForeignKey, IntegerField, Model)
+from .managers import UserManager
 
 # Create your models here.
 
 
 class Account(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
     username = None
     first_name = None
     last_name = None
     confirmed = BooleanField(default=False)
     email = EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
 
 class Subscription(Model):
@@ -26,6 +29,9 @@ class Subscription(Model):
     city = ForeignKey('City', on_delete=CASCADE)
     notification_period = IntegerField(choices=NOTIFICATION_PERIODS)
 
+    class Meta:
+        ordering = ['-id']
+
 
 class City(Model):
     id = IntegerField(primary_key=True, unique=True)
@@ -33,6 +39,9 @@ class City(Model):
     state_code = CharField(max_length=9, null=True)
     country = CharField(max_length=100, null=True)
     country_code = CharField(max_length=3, null=True)
+
+    class Meta:
+        ordering = ['-id']
 
     def __str__(self):
         return f'< City : {self.name} >'
