e0c63228a5cbcd0053256b65cb7bb6f3e4af1327
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-07 02:10:38 +0500

    Token generator added and partial update puted tothe PUT method

--- DjangoWeatherRemider/api/utils.py
+++ DjangoWeatherRemider/api/utils.py
@@ -1,9 +1,33 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
                                    ListModelMixin, UpdateModelMixin)
-
+from nanoid import generate
 
 # Create your classes here.
+
+
+class TokenGenerator(object):
+    tokens_storage = list()
+
+    def __init__(self, account):
+        self.token = generate(size=40).replace(' ', '-')
+        self.account = account
+
+    @classmethod
+    def check_token(cls, account, token: str) -> bool:
+        for token_obj in cls.tokens_storage:
+            if token_obj.token == token and token_obj.account == account:
+                cls.tokens_storage.remove(token_obj)
+                return True
+        return False
+
+    @classmethod
+    def make_token(cls, account) -> str:
+        obj = TokenGenerator(account)
+        cls.tokens_storage.append(obj)
+        return obj.token
+
+
 class CLUDAPIView(
     GenericAPIView, CreateModelMixin, DestroyModelMixin,
     ListModelMixin, UpdateModelMixin,
@@ -15,7 +39,7 @@ class CLUDAPIView(
         return self.list(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+        return self.partial_update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
