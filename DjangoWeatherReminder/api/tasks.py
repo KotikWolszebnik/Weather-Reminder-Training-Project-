281c2efc34ece632071389eb2481e434940eb693
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-10 15:19:41 +0500

    task added

--- DjangoWeatherReminder/api/tasks.py
+++ DjangoWeatherReminder/api/tasks.py
@@ -1,1 +1,11 @@
 from celery import shared_task
+
+from .models import Subscription
+from .utils import send_remind
+
+
+@shared_task
+def send_weather_notification(period: int):
+    subscriptions = Subscription.objects.filter(notification_period=period)
+    for subscription in subscriptions:
+        send_remind(subscription.user, subscription.city.id)
