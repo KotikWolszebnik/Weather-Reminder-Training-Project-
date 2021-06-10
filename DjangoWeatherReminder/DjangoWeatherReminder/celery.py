456d30f7e30e824b24062253d336e269964e9240
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-10 15:20:49 +0500

    schedule and schedule generating function are added

--- DjangoWeatherReminder/DjangoWeatherReminder/celery.py
+++ DjangoWeatherReminder/DjangoWeatherReminder/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWeatherReminder.settings')
@@ -17,6 +18,19 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+def generate_shedule() -> dict:
+    shedule = dict()
+    for period in [1, 3, 6, 12, 24]:
+        if period == 24:
+            hour = 0
+        else:
+            hour = period
+        shedule[f'run-every {period}'] = dict(
+            task='tasks.send_weather_notification',
+            schedule=crontab(hour=hour),
+            kwargs=dict(period=period),
+        )
+    return shedule
+
+
+app.conf.beat_schedule = generate_shedule()
