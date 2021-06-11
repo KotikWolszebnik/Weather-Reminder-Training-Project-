1bf1bd5d6b03ae1ab1b117bfbd8017415eb42a58
Shavrin Maksim <nutmegraw@gmail.com>
2021-06-11 13:39:23 +0500

    methods deleted

--- DjangoWeatherReminder/api/views.py
+++ DjangoWeatherReminder/api/views.py
@@ -23,18 +23,6 @@ class SubscriptionView(ModelViewSet):
     serializer_class = SubscriptionSerializer
     permission_classes = [IsAuthenticated & IsConfirmed & IsOwner | IsAdminUser]
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
     def get_queryset(self):
         return Subscription.objects.filter(user=self.request.user)
 
